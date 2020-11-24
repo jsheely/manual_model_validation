e45849a327f1c2639a2e52cce67f754897415787
Jonathan Sheely <jsheely@thirtytech.net>
2020-11-24 16:07:04 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace model_validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new TestModel
+            {
+                Name = "Wow. That totally works!"
+            };
+
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(data, new ValidationContext(data), results, true);
+        }
+    }
+    public class TestModel
+    {
+        [MaxLength(10)]
+        public string Name { get; set; }
+    }
+}
